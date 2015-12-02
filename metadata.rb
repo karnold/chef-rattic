989f5993713472dae6f80387f2785317f1c65062
Kristen Arnold <karnold@sfmoma.org>
2015-12-02 13:55:10 -0800

    updated version number

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'karnold@sfmoma.org'
 license          'All rights reserved'
 description      'Installs/Configures rattic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "apache2"
 depends "database"
